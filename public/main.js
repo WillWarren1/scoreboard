5ae2efa582db1ccfdac1fc5974fd92489ef60870
Will Warren <willwarrenmail@gmail.com>
2019-02-05 16:19:32 -0500

    finished

--- public/main.js
+++ public/main.js
@@ -3,93 +3,132 @@ let counter1 = 0
 let counter2 = 0
 
 // team name values
-let teamName1 = document.querySelector('.team1name').value
-let teamName2 = document.querySelector('.team2name').value
-// let newName1 = document.querySelector('team1new').value
-// let newName2 = document.querySelector('team2new').value
+let teamName1 = document.querySelector('.team1name').textContent
+let teamName2 = document.querySelector('.team2name').textContent
 
 // let scores be 0 upon opening page
 const main = () => {
-	document.querySelector('.team1Score').textContent = 0
-	document.querySelector('.team2Score').textContent = 0
+  document.querySelector('.team1Score').textContent = 0
+  document.querySelector('.team2Score').textContent = 0
+  counter1 = 0
+  counter2 = 0
+  document.querySelector('article').classList.remove('winner')
+  document.querySelector('article').classList.add('popup')
 }
 
 const updateName1 = () => {
-	teamName1 = document.querySelector('.team1new').value
-	document.querySelector('.team1name').textContent = teamName1
+  if (counter1 !== 21 && counter2 !== 21) {
+    teamName1 = document.querySelector('.team1new').value
+    document.querySelector('.team1name').textContent = teamName1
+  }
 }
 const updateName2 = () => {
-	teamName2 = document.querySelector('.team2new').value
-	document.querySelector('.team2name').textContent = teamName2
+  if (counter1 !== 21 && counter2 !== 21) {
+    teamName2 = document.querySelector('.team2new').value
+    document.querySelector('.team2name').textContent = teamName2
+  }
 }
 
 //when a user clicks a button
 const increaseCounterOne = () => {
-	// do things
-	console.log('button clicked')
-	// increase the variable counter1 by one
-	counter1++
-	console.log(counter1)
-	// update the html with the new value
-	document.querySelector('.team1Score').textContent = counter1
+  // do things
+  console.log('button clicked')
+  if (counter1 < 21 && counter2 !== 21) {
+    counter1++
+  } else {
+    return
+  }
+  document.querySelector('.team1Score').textContent = counter1
+  if (counter1 === 21) {
+    document.querySelector('article').classList.add('winner')
+    document.querySelector('article').classList.remove('popup')
+    document.querySelector('.victor').textContent = teamName1
+
+    // document.querySelector('p.team2name').classList.add('popup')
+  }
+
+  // if (teamName1 !== null) {
+  // 	document.querySelector('victor').textContent = teamName1
+  // } else {
+  // 	document.querySelector('victor').textContent = Team 1
+  // }
+  console.log(counter1)
+  // update the html with the new value
 }
 
 // when a user clicks a button
 const decreaseCounterOne = () => {
-	// do things
-	console.log('button clicked')
-	// increase the variable counter1 by one
-	counter1 = counter1 - 1
-	console.log(counter1)
-	// update the html with the new value
-	document.querySelector('.team1Score').textContent = counter1
+  // do things
+  console.log('button clicked')
+  if (counter1 > 0 && counter1 !== 21 && counter2 !== 21) {
+    counter1 = counter1 - 1
+  } else {
+    return
+  }
+  console.log(counter1)
+  // update the html with the new value
+  document.querySelector('.team1Score').textContent = counter1
 }
 
 // counter 2 stuff
 // when a user clicks a button
 const increaseCounterTwo = () => {
-	// do things
-	console.log('button clicked')
-	// increase the variable counter1 by one
-	counter2++
-	console.log(counter2)
-	// update the html with the new value
-	document.querySelector('.team2Score').textContent = counter2
+  // do things
+  console.log('button clicked')
+  if (counter2 < 21 && counter1 !== 21) {
+    counter2++
+  } else {
+    return
+  }
+  if (counter2 == 21) {
+    document.querySelector('article').classList.add('winner')
+    document.querySelector('article').classList.remove('popup')
+    document.querySelector('.victor').textContent = teamName2
+  }
+  console.log(counter2)
+  // update the html with the new value
+  document.querySelector('.team2Score').textContent = counter2
 }
 
 // when a user clicks a button
 const decreaseCounterTwo = () => {
-	// do things
-	console.log('button clicked')
-	// decrease the variable counter1 by one
-	counter2 = counter2 - 1
-	console.log(counter2)
-	// update the html with the new value
-	document.querySelector('.team2Score').textContent = counter2
+  // do things
+  console.log('button clicked')
+  // decrease the variable counter1 by one
+  if (counter2 > 0 && counter2 !== 21 && counter1 !== 21) {
+    counter2 = counter2 - 1
+  } else {
+    return
+  }
+  console.log(counter2)
+  // update the html with the new value
+  document.querySelector('.team2Score').textContent = counter2
 }
 
 document
-	.querySelector('.update-team-1-name')
-	.addEventListener('click', updateName1)
+  .querySelector('.update-team-1-name')
+  .addEventListener('click', updateName1)
 
 document
-	.querySelector('.update-team-2-name')
-	.addEventListener('click', updateName2)
+  .querySelector('.update-team-2-name')
+  .addEventListener('click', updateName2)
 
 document
-	.querySelector('.team-1-add-1-button')
-	.addEventListener('click', increaseCounterOne)
+  .querySelector('.team-1-add-1-button')
+  .addEventListener('click', increaseCounterOne)
 
 document
-	.querySelector('.team-1-subtract-1-button')
-	.addEventListener('click', decreaseCounterOne)
+  .querySelector('.team-1-subtract-1-button')
+  .addEventListener('click', decreaseCounterOne)
 
 document
-	.querySelector('.team-2-add-1-button')
-	.addEventListener('click', increaseCounterTwo)
+  .querySelector('.team-2-add-1-button')
+  .addEventListener('click', increaseCounterTwo)
 
 document
-	.querySelector('.team-2-subtract-1-button')
-	.addEventListener('click', decreaseCounterTwo)
+  .querySelector('.team-2-subtract-1-button')
+  .addEventListener('click', decreaseCounterTwo)
 
 document.addEventListener('DOMContentLoaded', main)
+
+document.querySelector('.reset').addEventListener('click', main)
